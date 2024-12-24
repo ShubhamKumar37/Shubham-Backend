82c019d85dfbd09db39fdb241f22c8f6a78a602a
ShubhamKumar37 <sk9818281820@gmail.com>
2024-12-25 01:27:08 +0530

    resuming the backend project

--- src/app.js
+++ src/app.js
@@ -10,9 +10,9 @@ app.use(cors({
     credentials: true,
 }));
 
-app.use(express.json({limit: "16Kb"}));    
+app.use(express.json({limit: "16Kb"})); // Limit specify the size of the request from front end
 app.use(express.urlencoded({extended: true, limit: "16Kb"}));
-app.use(express.static("public"));    
+app.use(express.static("public"));    // A place where some assest are stored
 app.use(cookieparser())
 
 export {app};   
--- src/index.js
+++ src/index.js
@@ -8,4 +8,4 @@ dbConnect()
 .then(() => app.listen(process.env.PORT || 8000, () => console.log(`Server is running of http://localhost:${process.env.PORT || 8000}`)))
 .catch((error) => console.log("There is error DB connection = ", error));
 
-app.on("error", (error) => console.log("There is while initializing server :: ", error))
+app.on("error", (error) => console.log("There is while initializing server :: ", error));
